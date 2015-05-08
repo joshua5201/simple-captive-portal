7b3a31c62abf709ca875d3ad68fbf91be44fa553
Tsung-en Hsiao <joshua841025@gmail.com>
2015-05-08 21:20:56 +0800

    update rules

--- auth_portal.sh
+++ auth_portal.sh
@@ -40,8 +40,8 @@ do
                 #requirement of NA course (redirecting to proxy)
                 iptables -t nat -I PREROUTING 1 -s $authipaddr -p tcp --dport 80 -j REDIRECT --to-ports 3128                  
                 #iptables -t nat -I PREROUTING 1 -s $authipaddr -p tcp --dport 80 -j ACCEPT 
-                #iptables -t nat -I PREROUTING 1 -s $authipaddr -p tcp --dport 443 -j ACCEPT
                 iptables -t filter -I FORWARD 1 -s $authipaddr -p tcp --dport 443 -j ACCEPT
+                iptables -t nat -I POSTROUTING 1 -o $extif -j MASQUERADE
                 passed=true
                 echo "auth passed"
                 break
